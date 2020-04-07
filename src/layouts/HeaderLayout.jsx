e932dfe6e46125616867097d9dfb7ffe28e5f23d
unknown <jolinacoronel04@gmail.com>
2020-04-07 22:21:27 +0800

    Updated react version and added routes

--- src/layouts/HeaderLayout.jsx
+++ src/layouts/HeaderLayout.jsx
@@ -1,32 +1,38 @@
 import React from "react";
 import { Navbar, Container, NavbarBrand, Collapse, Nav, NavItem, NavLink } from 'reactstrap';
 
+import { useHistory } from "react-router-dom";
 
-class HeaderLayout extends React.Component {
-  render() {
-    return (
-      <Navbar expand="lg" color="primary">
-          <Container>
-              <NavbarBrand>MERN-FOOD</NavbarBrand>
-              <Collapse navbar>
-                  <Nav navbar>
-                      <NavItem active>
-                          <NavLink to="/login">
-                              Login <span className="sr-only">(current)</span>
-                          </NavLink>
-                      </NavItem>
-                      
-                      <NavItem>
-                          <NavLink to="/">
-                              Register
-                          </NavLink>
-                      </NavItem>
-                  </Nav>
-              </Collapse>
-          </Container>
-      </Navbar>
-    );
+
+function HeaderLayout () {
+  let history = useHistory();
+
+  const gotoRoute = (path) => () => {
+    history.push(path);
   }
+
+  return (
+    <Navbar expand="lg" color="primary">
+        <Container>
+            <NavbarBrand>MERN-FOOD</NavbarBrand>
+            <Collapse navbar>
+                <Nav navbar>
+                    <NavItem active>
+                        <NavLink onClick={gotoRoute("/login")}>
+                            Login <span className="sr-only">(current)</span>
+                        </NavLink>
+                    </NavItem>
+                    
+                    <NavItem>
+                    <NavLink onClick={gotoRoute("/")}>
+                            Register
+                        </NavLink>
+                    </NavItem>
+                </Nav>
+            </Collapse>
+        </Container>
+    </Navbar>
+  );
 }
 
 export default HeaderLayout;
